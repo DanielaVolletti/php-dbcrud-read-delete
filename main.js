cdfeeaa3379f1337aaf4f007a0b242f93486fc6e
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-05-29 15:39:50 +0200

    delete paganti

---
+++ main.js
@@ -0,0 +1,52 @@
+// funzioni
+function getAllPaganti() {
+  $.ajax({
+    url: "getAllPaganti.php",
+    method: "GET",
+    success: function (data) {
+      printPagante (data);
+    },
+    error: function (err) {
+      console.error(err);
+    }
+  });
+};
+
+function printPagante (data) {
+  // inizializzo handlebars
+  var source = $("#pagante-template").html();
+  var template = Handlebars.compile(source);
+  // estrapolo valori del pagante e li stampo in pagina
+  for (var pagante of data) {
+    var html = template(pagante);
+    $('.paganti').append(html);
+  }
+
+};
+
+function deletePagante() {
+  var cestino = $(this);
+  var paganteSelezionato = cestino.parent();
+  var id = paganteSelezionato.data('id');
+  $.ajax({
+    url: "deletePaganti.php",
+    method: "POST",
+    data: {
+      id: id
+    },
+    success: function () {
+      paganteSelezionato.remove();
+    },
+    error: function (err) {
+      console.error(err);
+    }
+  })
+}
+
+
+$(document).ready(function () {
+
+  getAllPaganti();
+
+  $(document).on("click", "i.delete", deletePagante);
+})
